0a74570e0db433267e719c4d6a4408aee16670a8
Martin Egeskov <mart337i@gmail.com>
2025-01-19 19:33:24 +0100

    [ADD] Add licence

--- addons/test_module/cli/test.py
+++
@@ -1,7 +0,0 @@
-from clix import Command
-
-class TEST(Command):
-    """Greet the user with a friendly message."""
-    def run(self, args: list[str]) -> None:
-        name = args[0] if args else "world"
-        print(f"Hello, {name}!")
